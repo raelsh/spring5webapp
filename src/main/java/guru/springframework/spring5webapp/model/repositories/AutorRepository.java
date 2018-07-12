eefd0e8e9b513bbdc3b607e5bf654fd4aae38708
Raelsh <raulsegher@gmail.com>
2018-07-11 23:33:41 -0500

    Spring Data JPA Repositories

---
+++ src/main/java/guru/springframework/spring5webapp/model/repositories/AutorRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5webapp.model.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.spring5webapp.model.Autor;
+
+public interface AutorRepository extends CrudRepository<Autor, Long> {
+
+}
---
+++ src/main/java/guru/springframework/spring5webapp/model/repositories/LibroRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5webapp.model.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.spring5webapp.model.Libro;
+
+public interface LibroRepository extends CrudRepository<Libro, Long> {
+
+}
+	
